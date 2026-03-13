8de93db22417835a21911b1b5b0c6d6dcd6cb364
g4sacs@gmail.com <c.acs@commend.at>
2026-03-13 06:43:35 +0100

    Update FlexViewWorkspacePlugin.cs

--- Smart Client Plugins/FlexView/Client/FlexViewWorkspacePlugin.cs
+++ Smart Client Plugins/FlexView/Client/FlexViewWorkspacePlugin.cs
@@ -7,13 +7,13 @@ namespace FlexView.Client
     public class FlexViewWorkspacePlugin : WorkSpacePlugin
     {
         public override Guid Id => FlexViewDefinition.WorkspacePluginId;
-        public override string Name => "FlexView";
+        public override string Name => "Flex View";
 
         public override void Init()
         {
             try
             {
-                FlexViewDefinition.Log.Info($"WorkspacePlugin Init - ViewAndLayoutItem is {(ViewAndLayoutItem == null ? "null" : "not null")}");
+                FlexViewDefinition.Log.Info($"WorkspacePlugin Init");
 
                 // Set layout to a single full-size slot so no stale camera slots remain
                 ViewAndLayoutItem.Layout = new Rectangle[]
@@ -22,7 +22,7 @@ namespace FlexView.Client
                 };
 
                 ViewAndLayoutItem.InsertViewItemPlugin(0, new FlexViewViewItemPlugin(), null);
-                FlexViewDefinition.Log.Info("WorkspacePlugin InsertViewItemPlugin succeeded");
+                FlexViewDefinition.Log.Info("WorkspacePlugin succeeded");
             }
             catch (Exception ex)
             {
